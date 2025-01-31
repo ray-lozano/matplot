4229d140d14245674eb6186543f9ca7128b9b95a
Raymond Lozano <ray-lozano@live.com>
2025-01-31 07:41:30 -0800

    Refactored

--- rolling_two_dice/dice_visual.py
+++ rolling_two_dice/dice_visual.py
@@ -7,18 +7,18 @@ die_1 = Die()
 die_2 = Die()
 
 # Make some rolls, and store restuls in a list.
-results = []
-for roll_num in range(1000):
-    result = die_1.roll() + die_2.roll()
-    results.append(result)
+results = [die_1.roll() + die_2.roll() for roll_num in range(1000)]
+#for roll_num in range(1000):
+ #   result = die_1.roll() + die_2.roll()
+  #  results.append(result)
 
 # Analyze the results.
-frequencies = []
 max_result = die_1.num_sides + die_2.num_sides
 poss_results = range(2, max_result+1)
-for value in poss_results:
-    frequency = results.count(value)
-    frequencies.append(frequency)
+frequencies = [results.count(value) for value in poss_results]
+#for value in poss_results:
+#    frequency = results.count(value)
+#    frequencies.append(frequency)
 
 # Visualize the reults.
 title = "Results of Rolling Two D6 1,000 Times"
